ecd75ffd7afdbe390c244ab32530724c3110d037
helewrer3 <helewrer3@gmail.com>
2021-01-23 00:26:54 +0530

    Fixing deployment

--- src/public/app/all-posts.js
+++ src/public/app/all-posts.js
@@ -27,7 +27,6 @@ async function loadPosts() {
                         body: body
                     }, (data) => {
                         console.log('created');
-                        $('#comment-body').empty();
                         window.alert('Comment Successfully created! Refresh to view changes.');
                     })
                 })
